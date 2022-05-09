7fc84efb55791be01558c51da0c6a51271551dbe
Vadym <izyadovakinovich@gmail.com>
2022-05-09 03:14:56 +0300

    Lab_3

---
+++ Lab_3/Lab_3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_3
+{
+    class Number
+    {
+        public int value = 0;
+        public void getValue()
+        {
+            Console.WriteLine(this.value);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Number> numbers = new List<Number>();
+            numbers.Add(new Number());
+            numbers.First().getValue();
+            numbers.First().value = 10;
+            numbers.First().getValue();
+            numbers.Remove(numbers.First());
+            Console.ReadKey();
+
+        }
+    }
+}
